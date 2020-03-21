dcced8845a8ec5a2ff9c1905ecb20afe354e64b9
Andrei <shakovskiyandrei@gmail.com>
2020-03-22 01:58:26 +0300

    Initial commit

---
+++ ProtocolPOP3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+
+namespace ProtocolPOP3 {
+    internal class Program {
+        public static void Main(string[] args) {
+            TcpClient tcpClient = new TcpClient();               
+            tcpClient.Connect("pop.mail.ru", 995);
+            SslStream sslStream = new SslStream(tcpClient.GetStream());
+            sslStream.AuthenticateAsClient("pop.mail.ru");
+
+            StreamWriter streamWriter = new StreamWriter(sslStream);
+            StreamReader streamReader = new StreamReader(sslStream);
+            
+            streamWriter.WriteLine("USER andrei.shpakovskiy@mail.ru");
+            streamWriter.Flush();
+            
+            streamWriter.WriteLine("PASS 98765432one");
+            streamWriter.Flush();
+            
+            streamWriter.WriteLine("STAT");
+            streamWriter.Flush();
+            
+            streamWriter.WriteLine("LIST 1");
+            streamWriter.Flush();
+
+            streamWriter.WriteLine("RETR 4");
+            streamWriter.Flush();
+            
+            streamWriter.WriteLine("QUIT");
+            streamWriter.Flush();
+
+            string receivedInfo = ""; 
+            string line = "";
+            
+            while ((line = streamReader.ReadLine()) != null) {
+                receivedInfo += line + "\n";
+            }
+
+            Console.Write(receivedInfo);
+            Console.Read();
+        }
+    }
+}
