50ba1e5f4d1e363abf98d4ed5eebd0719f90cd62
Andrei <shakovskiyandrei@gmail.com>
2020-03-22 22:56:42 +0300

    Comments added

--- ProtocolPOP3/Program.cs
+++ ProtocolPOP3/Program.cs
@@ -7,39 +7,41 @@ namespace ProtocolPOP3 {
     internal class Program {
         public static void Main(string[] args) {
             TcpClient tcpClient = new TcpClient();               
-            tcpClient.Connect("pop.mail.ru", 995);
+            tcpClient.Connect("pop.mail.ru", 995);        //Подключаем клиента к порту 995
             SslStream sslStream = new SslStream(tcpClient.GetStream());
             sslStream.AuthenticateAsClient("pop.mail.ru");
 
-            StreamWriter streamWriter = new StreamWriter(sslStream);
+            StreamWriter streamWriter = new StreamWriter(sslStream);    //Настраиваем поток воода и вывода
             StreamReader streamReader = new StreamReader(sslStream);
             
-            streamWriter.WriteLine("USER YOUR_EMAIL");
+            streamWriter.WriteLine("USER andrei.shpakovskiy@mail.ru");  //Отправляем на сервер адрес почты.  
+            streamWriter.Flush();                                       //Так как оно там все такое умное и кеширует,
+                                                                        //чтобы отправить большим куском, принудительно
+                                                                        //чистим этот буфер.
+            
+            streamWriter.WriteLine("PASS 98765432one");                 //То же самое с паролем   
             streamWriter.Flush();
             
-            streamWriter.WriteLine("PASS YOUR_PASSWORD");
+            streamWriter.WriteLine("STAT");                             //Запрос на количество сообщений в ящике 
+            streamWriter.Flush();                                       //и их размер ящика в октетах 
+            
+            streamWriter.WriteLine("LIST 1");                           //Запрашиваем инфу по сообщению с номером 1 
             streamWriter.Flush();
             
-            streamWriter.WriteLine("STAT");
+            streamWriter.WriteLine("RETR 4");                           //Запрашиваем сообщение с номером 4
             streamWriter.Flush();
             
-            streamWriter.WriteLine("LIST 1");
-            streamWriter.Flush();
-
-            streamWriter.WriteLine("RETR 4");
-            streamWriter.Flush();
-            
-            streamWriter.WriteLine("QUIT");
+            streamWriter.WriteLine("QUIT");                             //Закрываем соединение
             streamWriter.Flush();
 
             string receivedInfo = ""; 
             string line = "";
             
-            while ((line = streamReader.ReadLine()) != null) {
+            while ((line = streamReader.ReadLine()) != null) {          //Получаем ответы на то, что отослали серверу выше  
                 receivedInfo += line + "\n";
             }
 
-            Console.Write(receivedInfo);
+            Console.Write(receivedInfo);                                //И выводим на консоль
             Console.Read();
         }
     }
